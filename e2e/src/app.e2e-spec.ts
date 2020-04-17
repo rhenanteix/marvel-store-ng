d05eedb883226ce24af5af7850961efaf9ad7023
rhenan.teixeira <rhenan.teixeira@univision.net.br>
2020-04-17 17:49:25 -0300

    new configs

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('marvel app is running!');
+    expect(page.getTitleText()).toEqual('s app is running!');
   });
 
   afterEach(async () => {
