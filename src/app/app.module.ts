39bd4f22bac0cdda389ac5cac1fb77c80a3555c4
rhenan.teixeira <rhenan.teixeira@univision.net.br>
2020-04-18 14:55:47 -0300

    routes, navigation e paginas feitas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule }   from '@angular/forms';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+registerLocaleData(ptBr, 'pt');
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './navigation/menu/menu.component';
+
+import { FooterComponent } from './navigation/footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+import { APP_BASE_HREF } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { ComicDetailComponent } from './pages/comic-detail/comic-detail.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './pages/check-out/check-out.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HomeComponent,
+    ComicDetailComponent,
+    ShoppingCartComponent,
+    CheckOutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    [RouterModule.forRoot(rootRouterConfig, { useHash: false})]
   ],
-  providers: [],
+  providers: [
+    {provide: APP_BASE_HREF, useValue: '/'},
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: ptBr, useValue: 'pt' },
+    MenuComponent
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ComicDetailComponent } from './pages/comic-detail/comic-detail.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './pages/check-out/check-out.component';
+
+
+export const rootRouterConfig: Routes = [
+    { path:'', redirectTo:"/home", pathMatch:"full" },
+    { path:'home', component: HomeComponent },
+    { path:'comicDetail', component: ComicDetailComponent },
+    { path:'cart', component: ShoppingCartComponent },
+    { path:'check-out', component: CheckOutComponent }
+]
